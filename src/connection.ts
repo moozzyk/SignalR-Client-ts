da86e22eade1d95d78da767161368bcb015d3133
moozzyk <moozzyk@gmail.com>
2016-07-23 01:55:41 -0700

    Introducing state variable

--- src/connection.ts
+++ src/connection.ts
@@ -4,8 +4,20 @@ namespace signalR {
     }
 
     export class connection {
+        private _state : connectionState;
+
+        constructor() {
+            this._state = connectionState.disconnected;
+        }
+
         public get state(): connectionState {
-            return connectionState.disconnected;
+            return this._state;
+        }
+
+        public start() {
+            if (this._state !== connectionState.disconnected) {
+                throw new Error("Cannot start a connection that is not in the disconnected state.");
+            }
         }
     }
 }
